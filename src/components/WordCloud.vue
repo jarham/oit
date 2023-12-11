ebb835c351606962e891113e1436e9bf336c021b
Jari Hämäläinen <jari.hamalainen@symcode.fi>
2023-12-11 10:52:06 +0200

    WIP: improve initial positioning algorithm
    Scale to circle and use binary search to find nodes place.
    
    On old mini laptop / FF initial positioning of fi words now takes
    1200 - 1300 ms which is acceptable if we run it on web worker right
    after loading the page.

--- src/components/WordCloud.vue
+++ src/components/WordCloud.vue
@@ -232,8 +232,6 @@ const createNodes = () => {
         y: 0,
       },
       v: [],
-      vms: [],
-      vmsn: [],
     };
   });
 
@@ -244,30 +242,32 @@ const createNodes = () => {
 const positionNodes = () => {
   const tm1 = performance.now();
 
-  // Reposition nodes so that they're completely outside of the ellipse and that they don't collide.
-  nodes.forEach((n, i) => {
-    // width / 2 + minWidth / 2 = just outside the ellipse
-    // + minWidth * 2 adds margin
-    // + i * 2.5 * minWidth separates words from each other.
-    n.pos.x = width / 2 + nodeMaxWidth * (nodes.length + 1 + i) * 4;
-    n.pos.y = 0;
+  // Scale all to circle, use sy = y scaling factor (be sure to call updateDimensions() before this)
+  const sy = width / height;
+  const initNodes = nodes.map<WordNode>((n) => {
+    const n2 = cloneDeep(n);
+    n2.v.forEach((v) => (v.y *= sy));
+    return n2;
   });
-  // Start moving nodes to ellipse: Move each one from just outside of the ellipse
+
+  // Start moving nodes to ellipse: Move each one from min safe distance
   // towards the origin. Move as close to origin as possible without hitting other nodes.
   // Try from multiple angles (48 different ones).
+  const minSafeNodeDim = Math.max(nodeMaxWidth, nodeMaxHeight * sy) * 1.05;
+  let minSafeDist = minSafeNodeDim;
   const t1: Vec2 = {x: 0, y: 0};
   const t2: Vec2 = {x: 0, y: 0};
-  const t3: Vec2 = {x: 0, y: 0};
+  const closest: Vec2 = {x: 0, y: 0};
+  const zero: Vec2 = {x: 0, y: 0};
   const mdr: MinDistResult = {d: 0, v1i: 0, v2i: 0, minPoint: {x: 0, y: 0}};
   let ba: Body<WordNode>;
-  let bb: Body<WordNode>;
-  const rayStep = (2 * Math.PI) / 48;
-  let d: number;
+  const angleStep = (2 * Math.PI) / 48;
   let minD2: number;
-  // sim.initialize(nodes);
+  // farthest vertex distance^2 from origin
+  let maxD2 = Number.NEGATIVE_INFINITY;
   let simNodes: WordNode[] = [];
 
-  nodes.forEach((n, i) => {
+  initNodes.forEach((n, i) => {
     simNodes.push(n);
     sim.initialize(simNodes);
     ba = sim.findBody(n)!;
@@ -275,58 +275,103 @@ const positionNodes = () => {
       // Shortcut for the first one: Jump to origin
       n.pos.x = 0;
       n.pos.y = 0;
-      return;
-    }
+    } else {
+      minD2 = Number.POSITIVE_INFINITY;
+      for (let a = 0; a < 2 * Math.PI; a += angleStep) {
+        // Binary search for closest possible positions from this angle
+        // Initial position
+        n.pos.x = minSafeDist * Math.cos(a);
+        n.pos.y = minSafeDist * Math.sin(a);
+        // binary search step vector
+        t1.x = n.pos.x;
+        t1.y = n.pos.y;
+        // step vector multiplier
+        let m = -0.5;
+        // last known good position
+        t2.x = n.pos.x;
+        t2.y = n.pos.y;
 
-    // Line cast
-    minD2 = Number.POSITIVE_INFINITY;
-    for (let a = 0; a < 2 * Math.PI; a += rayStep) {
-      // Initial position
-      n.pos.x = (width / 2 + nodeMaxWidth / 2) * Math.cos(a);
-      n.pos.y = (height / 2 + nodeMaxHeight / 2) * Math.sin(a);
-      // console.log(`Node ${n.word} init pos: (${n.pos.x}, ${n.pos.y})`);
-      // Initial move vector
-      d = Math.sqrt(n.pos.x * n.pos.x + n.pos.y * n.pos.y);
-      t1.x = (n.pos.x / d) * (Math.min(nodeMaxHeight, nodeMaxWidth) / -2);
-      t1.y = (n.pos.y / d) * (Math.min(nodeMaxHeight, nodeMaxWidth) / -2);
-
-      while (true) {
-        sim.eng.checkBodyCollision(ba, true);
-        if (sim.eng.collisionCount > 0) {
-          // Step back if we hit
-          n.pos.x -= t1.x;
-          n.pos.y -= t1.y;
-
-          // Stop if move vector is already small enough
-          if (Math.abs(t1.x) < 0.8 && Math.abs(t1.y) < 0.8) {
-            break;
-          }
-
-          // Divide move vector to half
-          t1.x /= 2;
-          t1.y /= 2;
-        } else {
-          // Keep moving if no hits
+        let wasHit = false;
+        while (Math.abs(t1.x) > 0.8 || Math.abs(t1.y) > 0.8) {
+          t1.x *= m;
+          t1.y *= m;
+          m = 0.5;
           n.pos.x += t1.x;
           n.pos.y += t1.y;
+
+          sim.eng.checkBodyCollision(ba, true);
+          const hit = sim.eng.collisionCount > 0;
+          if (!hit) {
+            t2.x = n.pos.x;
+            t2.y = n.pos.y;
+          }
+
+          // If hit status changed, turn around
+          if (wasHit != hit) {
+            m = -0.5;
+            wasHit = hit;
+          }
+        }
+        // Set to last known good location
+        n.pos.x = t2.x;
+        n.pos.y = t2.y;
+
+        // Record min dist to origin
+        sim.eng.pointToBodyMinDist(zero, sim.eng.findBody(n.id)!, mdr);
+        if (mdr.d < minD2) {
+          minD2 = mdr.d;
+          closest.x = n.pos.x;
+          closest.y = n.pos.y;
         }
       }
 
-      // Record min dist to origin
-      sim.eng.pointToBodyMinDist(t2, sim.eng.findBody(n.id)!, mdr);
-      if (mdr.d < minD2) {
-        minD2 = mdr.d;
-        t3.x = n.pos.x;
-        t3.y = n.pos.y;
-        // console.log(`Node ${n.word} suggested pos: (${n.pos.x}, ${n.pos.y})`);
-      }
+      // Use the position that was closest to origin
+      n.pos.x = closest.x;
+      n.pos.y = closest.y;
     }
 
-    // Use the position that was closest to origin
-    n.pos.x = t3.x;
-    n.pos.y = t3.y;
-    // console.log(`Node ${n.word} final pos: (${n.pos.x}, ${n.pos.y})`);
+    // Update min safe dist: vertex farthest from origin + minSafeNodeDim
+    let updateMinSafeDist = false;
+    n.v.forEach((v) => {
+      t1.x = n.pos.x + v.x;
+      t1.y = n.pos.y + v.y;
+      const d2 = t1.x * t1.x + t1.y * t1.y;
+      if (d2 > maxD2) {
+        maxD2 = d2;
+        updateMinSafeDist = true;
+      }
+    });
+    if (updateMinSafeDist) {
+      minSafeDist = Math.sqrt(maxD2) + minSafeNodeDim;
+    }
   });
+
+  // Scale to fit
+  const d = Math.sqrt(maxD2);
+  const hw = width / 2 - props.viewportPadding.x;
+  const f = hw / d;
+  if (f > 1)
+  initNodes.forEach((n) => {
+    n.pos.x *= f;
+    n.pos.y *= f;
+  });
+
+
+  // Scale positions back to match ellipse
+  for (let i = 0; i < nodes.length; i++) {
+    const nInit = initNodes[i];
+    const n = nodes[i];
+    n.pos.x = nInit.pos.x;
+    n.pos.y = nInit.pos.y / sy;
+  }
+
+  // Few sim rounds to ensure positions are good
+  sim.initialize(nodes);
+  for (let i = 0; i < 80; i++) {
+    sim.tick();
+  }
+  sim.reset();
+
   const tm2 = performance.now();
   console.log(`Initial positioning took: ${tm2 - tm1} ms`);
 
